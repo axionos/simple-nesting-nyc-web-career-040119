f9c204114bdaee3bbeff671deb99f962e2511912
SOYEONG OH <axionoso@gmail.com>
2019-03-25 22:39:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,9 +102,8 @@ def changing_alan
      }
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-
-
 end
+changing_alan
 
 def adding_to_dennis
 	programmer_hash =
