98b0b4a3d1a2cf688f6b0ebe4d19e27615c760eb
SOYEONG OH <axionoso@gmail.com>
2019-03-25 22:47:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,10 +80,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-	programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-	programmer_hash[:yukihiro_matsumoto][:languages] = "LISP"
-	programmer_hash[:yukihiro_matsumoto][:languages] = "C" 
-
+	programmer_hash[:yukihiro_matsumoto] = {
+	  :known_for => "Ruby",
+	  :languages => ["LISP", "C"]
+	}
 end
 
 def changing_alan
